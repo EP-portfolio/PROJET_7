c1dbd6ae705b4ac69fe5439bc31a7c0e416defe5
EP-portfolio <m.eddyponton@gmail.com>
2025-02-17 15:54:03 +0100

    FIX : chemins relatifs 2

--- api/api.py
+++ api/api.py
@@ -7,8 +7,6 @@ import os
 
 port = int(os.getenv("PORT", 8000))
 
-path = r"C:\Users\lenovo\Desktop\OpenClassrooms\PROJET_OC\PROJET_7\PROJET_7"
-
 # Initialisation de l'API
 app = FastAPI(title="API Prédiction Crédit")
 
@@ -16,14 +14,14 @@ app = FastAPI(title="API Prédiction Crédit")
 base_dir = Path(__file__).parent.parent  # Remonte d'un niveau depuis le dossier api
 model_path = base_dir / "models" / "credit_model.joblib"
 scaler_path = base_dir / "models" / "scaler.joblib"
+data_path = base_dir / "DF_final_train.csv"  # Ajouté cette ligne
 
 # Chargement du modèle et du scaler
-model = joblib.load(path + r"\models\credit_model.joblib")
-scaler = joblib.load(path + r"\models\scaler.joblib")
+model = joblib.load(model_path)
+scaler = joblib.load(scaler_path)
 
 # Chargement des données avec SK_ID_CURR comme index
-df = pd.read_csv(path + r"\DF_final_train.csv", index_col="SK_ID_CURR")
-
+df = pd.read_csv(data_path, index_col="SK_ID_CURR")
 MIN_ID = df.index.min()
 MAX_ID = df.index.max()
 
@@ -37,21 +35,16 @@ async def predict(client_id: int):
                 status_code=400,
                 detail=f"L'ID client doit être compris entre {MIN_ID} et {MAX_ID} inclus",
             )
-
         # Récupérer les données du client
         client_data = df.loc[[client_id]]
-
         # Préparation des données (exclure TARGET si présente)
         if "TARGET" in client_data.columns:
             client_data = client_data.drop("TARGET", axis=1)
-
         # Scaling des données
         client_scaled = scaler.transform(client_data)
-
         # Prédiction et probabilité
         probability = float(model.predict_proba(client_scaled)[0][1])
         prediction = 1 if probability >= 0.36 else 0  # Utilisation du seuil optimal
-
         # Préparation de la réponse
         response = {
             "client_id": client_id,
@@ -59,9 +52,7 @@ async def predict(client_id: int):
             "prediction": prediction,
             "decision": "Crédit Refusé" if prediction == 1 else "Crédit Accepté",
         }
-
         return response
-
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
